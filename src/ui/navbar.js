688ae974e9a81d8231ac7f1fc1cfc2ad7c19a6ea
H0ck3y-QANTAS <fleishmans708@mysaddleback.org>
2026-02-11 13:18:51 -0800

    Update navbar.js

--- src/ui/navbar.js
+++ src/ui/navbar.js
@@ -1,12 +1,16 @@
 export function renderNavbar() {
   return `
     <nav class="nav">
-      <a href="/pages/index.html">Home</a>
-      <a href="/pages/map.html">Map</a>
-      <a href="/pages/dashboard.html">Dashboard</a>
-      <a href="/pages/apply.html">Apply</a>
-      <a href="/pages/staff.html">Staff</a>
-      <button id="loginBtn">Login</button>
+      <div class="nav-left">
+        <a href="/pages/index.html" class="brand">Qantas VA</a>
+        <a href="/pages/map.html">Map</a>
+        <a href="/pages/dashboard.html">Dashboard</a>
+        <a href="/pages/apply.html">Apply</a>
+        <a href="/pages/staff.html">Staff</a>
+      </div>
+      <div class="nav-right">
+        <button id="loginBtn">Login</button>
+      </div>
     </nav>
   `;
 }
